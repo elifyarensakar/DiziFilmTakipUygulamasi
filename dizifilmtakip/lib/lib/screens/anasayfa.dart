0457effb476498fdbff608ddde8e3e190d1118bb
MelisaZobali <melisaazobali@gmail.com>
2025-05-22 21:06:30 +0300

    Anasayfa hata çözümü

--- dizifilmtakip/lib/lib/screens/anasayfa.dart
+++ dizifilmtakip/lib/lib/screens/anasayfa.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:dizifilmtakip/lib/screens/oneri_chatbot_ekrani.dart';
 import 'package:dizifilmtakip/lib/screens/icerik_ara_sayfasi.dart';
 import 'package:dizifilmtakip/lib/screens/profil_sayfasi.dart';
-import 'package:dizifilmtakip/lib/screens/oneri_chatbot_ekrani.dart';
 import 'package:dizifilmtakip/lib/screens/devam_tahmin_ekrani.dart';
 
 class AnaSayfa extends StatefulWidget {
@@ -19,7 +19,7 @@ class _AnaSayfaState extends State<AnaSayfa> {
   Widget build(BuildContext context) {
     final sayfalar = [
       OneriChatbotEkrani(),
-      IcerikAraSayfasi(kullaniciEmail: widget.kullaniciEmail),
+      AnaEkran(email: widget.kullaniciEmail),
       ProfilSayfasi(kullaniciEmail: widget.kullaniciEmail),
       DevamTahminEkrani(),
     ];
@@ -48,6 +48,33 @@ class _AnaSayfaState extends State<AnaSayfa> {
           BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.help_outline), label: ''),
         ],
+      ),
+    );
+  }
+}
+
+class AnaEkran extends StatelessWidget {
+  final String email;
+  const AnaEkran({required this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF03003F),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.movie, color: Colors.white, size: 80),
+            SizedBox(height: 16),
+            Text(
+              'Hoş geldin 👋',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+            SizedBox(height: 8),
+            Text(email, style: TextStyle(color: Colors.white70)),
+          ],
+        ),
       ),
     );
   }
